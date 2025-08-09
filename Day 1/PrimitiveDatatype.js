3160d520785982407874cc7e7a0aaf45584e2f69
lalitha-1987 <laake.sll87@gmail.com>
2025-08-10 01:33:51 +0530

    Day 1 Commit

---
+++ Day 1/PrimitiveDatatype.js
@@ -0,0 +1,15 @@
+let firstName ="LalithaLakshmi"
+let companyName="Acn"
+let mobileNumber =9923467845
+let isAutomation=true
+let hasPlaywright ;
+console.log(firstName)
+console.log("Datatype of FirstName is " +typeof firstName)
+console.log(companyName)
+console.log("Datatype of Company Name is " +typeof companyName)
+console.log(mobileNumber)
+console.log("Datatype of mobileNumber is " +typeof mobileNumber)
+console.log(isAutomation)
+console.log("Datatype of IsAutomation is " +typeof isAutomation)
+console.log(hasPlaywright)
+console.log("Datatype of HasPlaywright is " +typeof hasPlaywright)
